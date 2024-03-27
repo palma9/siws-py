f0dafd6ce43de80db74d4b7abd2c84949d8d24e5
Jesús Palma <jpalmalop@gmail.com>
2024-03-27 18:16:56 +0100

    fix: add build dependencies

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+dependencies = ["pydantic", "abnf", "python-dateutil", "ecdsa"]
+
 setuptools.setup(
     name="siws",
-    version="0.0.1",
+    version="0.0.2",
     author="Jesús Palma",
     author_email="jpalmalop@gmail.com",
     description="A python adaptation from Sign it with ethereum for Stacks",
@@ -18,4 +20,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=dependencies,
 )
